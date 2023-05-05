9a1201553c8e5caf567932091c02e95a31c33b41
Davide Capicchioni <116790994+Davycaps@users.noreply.github.com>
2023-05-05 08:48:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// 27/4/23
+//connessione al database
+
+using SQLite;
+
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+
+var tblArtists = cn1.Query<Artist>("select * from artists");
+
+Console.WriteLine( $"in questa tabella ci sono {tblArtists.Count} record");
+
+//4/5/23
+var temporanea = tblArtists.OrderByDescending(x => x.Name).Max( y = y.ArtistId );
+console.WriteLine( $"{temporanea}");
+
+foreach( var artista in tblArtist )
+{
+    console.WriteLine( $"{artista.Name}");
+}
+
+console.WriteLine( $"{temporanea}");
+//fine main... inizio dichiarazioni classi-----------------------
+public class Artist
+{
+    public int ArtistId{get;set;}
+    public string Name{get;set;}
+}
